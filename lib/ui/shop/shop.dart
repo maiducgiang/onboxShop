e81be513cfdd70ffc80e4372be95262116818b5a
Mabook Pro <>
2023-05-24 00:15:57 +0700

    update

--- lib/ui/shop/shop.dart
+++ lib/ui/shop/shop.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ShopScreen extends StatefulWidget {
   const ShopScreen({super.key});
@@ -11,6 +13,81 @@ class ShopScreen extends StatefulWidget {
 class _ShopScreenState extends State<ShopScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/background_shop.png',
+            fit: BoxFit.fill,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  height: 500,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      width: 1,
+                    ),
+                    itemImage(
+                        path: 'assets/images/box.png',
+                        title: "New Deal",
+                        onTap: () {}),
+                    itemImage(
+                        path: 'assets/images/computer.png',
+                        title: "Computer",
+                        onTap: () {},
+                        size: 90),
+                    itemImage(
+                        path: 'assets/images/camera.png',
+                        title: "Camera",
+                        onTap: () {},
+                        size: 100),
+                    itemImage(
+                        path: 'assets/images/iphone.png',
+                        title: "Phone",
+                        onTap: () {},
+                        size: 90),
+                    const SizedBox(
+                      width: 1,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+    ;
+  }
+
+  Widget itemImage(
+      {required String path,
+      required String title,
+      Function? onTap,
+      int size = 60}) {
+    return GestureDetector(
+      onTap: () {
+        onTap?.call();
+      },
+      child: Column(
+        children: [
+          Image.asset(
+            path,
+            width: size.toDouble(),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(title)
+        ],
+      ),
+    );
   }
 }
